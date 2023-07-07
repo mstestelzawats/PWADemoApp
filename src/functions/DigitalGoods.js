85aa55a83f06eda66696c9cba4ec5788413b026a
Elijah Zawatsky <elzawats@microsoft.com>
2023-07-07 16:13:57 -0700

    add js file

---
+++ src/functions/DigitalGoods.js
@@ -0,0 +1,73 @@
+export async function DigitalGoodsPurchase() {
+    if (window.getDigitalGoodsService === undefined) {
+        // Digital Goods API is not supported in this context.
+        alert("getDigitalGoodsService is undefined");
+        return;
+    }
+    try {
+        alert("getDigitalGoodsService is defined, attempting purchase");
+        const service = await window.getDigitalGoodsService('https://store.microsoft.com/billing');
+        const items = await service.getDetails(['SCCPWATestAppConsumable3']);
+        const request = new PaymentRequest([{
+            supportedMethods: 'https://store.microsoft.com/billing',
+            data: { itemId: items[0].itemId },
+        }]);
+        const response = await request.show();
+    } catch (error) {
+        console.error("Failed to get service:", error.message);
+        return;
+    }
+}
+
+export function DigitalGoodsConfirmPurchase() {
+    if (window.getDigitalGoodsService === undefined) {
+        // Digital Goods API is not supported in this context.
+        alert("getDigitalGoodsService is undefined");
+        return;
+    }
+    try {
+        alert("getDigitalGoodsService is defined, confirming purchase");
+        const service = window.getDigitalGoodsService('https://store.microsoft.com/billing');
+        purchases = service.listPurchases();
+        for (p of purchases) {
+            alert(p.itemId);
+            VerifyOnBackendAndGrantEntitlement(p.itemId, p.purchaseToken);
+        }
+    } catch (error) {
+        console.error("Failed to get service:", error.message);
+        return;
+    }
+}
+
+var value = 'TESTING';
+
+export function DigitalGoodsGetDetails() {
+    return value;
+    if (window.getDigitalGoodsService === undefined) {
+        // Digital Goods API is not supported in this context.
+        alert("getDigitalGoodsService is undefined");
+        return;
+    }
+    try {
+        alert("getDigitalGoodsService is defined, getting details");
+        const service = window.getDigitalGoodsService('https://store.microsoft.com/billing');
+        const details = digitalGoodsService.getDetails(['SCCPWATestAppSubscription1', 'Coins', 'RemoveAds']);
+        for (item of details) {
+            alert(p.itemId);
+            const priceStr = new Intl.NumberFormat(
+                locale,
+                {style: 'currency', currency: item.price.currency}
+              ).format(item.price.value);
+            alert(priceStr);
+            AddShopMenuItem(item.itemId, item.title, priceStr, item.description);
+        }
+    } catch (error) {
+        console.error("Failed to get service:", error.message);
+        return;
+    }
+}
+
+export function changecontent()
+{
+    alert("IN FUN");
+}
